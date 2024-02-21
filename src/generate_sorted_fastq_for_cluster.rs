ea02a14e69bdc32a44c17b73fa976541844b05eb
aljpetri <aljpetri@gmail.com>
2024-02-21 18:52:40 +0200

    Removed unused code fragments

--- src/generate_sorted_fastq_for_cluster.rs
+++ src/generate_sorted_fastq_for_cluster.rs
@@ -19,25 +19,6 @@ use bio::io::fastq::FastqRead;
 use rustc_hash::FxHashMap;
 
 //https://doc.rust-lang.org/std/primitive.char.html#method.decode_utf16  for parsing of quality values
-/*fn compress_sequence(seq: &str) -> String {
-    //compresses the sequence seq by keeping only the first character of each consecutive group of equal characters. The resulting compressed sequence is stored in the variable seq_hpol_comp.
-
-    let mut seq_hpol_comp = String::new();
-    let mut last_char: Option<char> = None;
-
-    for ch in seq.chars() {
-        if last_char.is_none() || last_char.unwrap() != ch {
-            seq_hpol_comp.push(ch);
-        }
-        last_char = Some(ch);
-    }
-
-    seq_hpol_comp
-}
-
- */
-
-
 fn compress_sequence(seq: &[u8]) -> String {
     //compresses the sequence seq by keeping only the first character of each consecutive group of equal characters. The resulting compressed sequence is stored in the variable seq_hpol_comp.
 
@@ -52,6 +33,7 @@ fn compress_sequence(seq: &[u8]) -> String {
 
     seq_hpol_comp
 }
+
 
 fn expected_number_errornous_kmers(quality_string: &str, k: usize, d:&[f64;128]) -> f64 {
     //computes the expected number of errornous kmers for a read by analysing the quality entry
@@ -97,6 +79,7 @@ fn calculate_error_rate(qual: &[u8], d_no_min: &[f64; 128]) -> f64 {
     error_rate
 }
 
+
 fn compute_d() -> [f64; 128] {
     let mut d = [0.0; 128];
 
@@ -108,6 +91,7 @@ fn compute_d() -> [f64; 128] {
     }
     d
 }
+
 
 //D_no_min = {chr(i) : 10**( - (ord(chr(i)) - 33)/10.0 )  for i in range(128)}
 fn compute_d_no_min() -> [f64; 128] {
@@ -121,212 +105,18 @@ fn compute_d_no_min() -> [f64; 128] {
     }
     d
 }
-/// Generates positional minimizers from an input string.
-/// A positional minimizer is the lexicographically smallest substring of a given window size
-/// as the window slides through the input string.
-///
-/// # Arguments
-///
-/// * `input` - The input string to generate minimizers from.
-/// * `window_size` - The size of the sliding window for generating minimizers.
-/// * `k` - The length of k-mers to use for generating minimizers.
-///
-/// # Returns
-///
-/// A vector containing `Minimizer` structs, each containing the lexicographically smallest
-///substring and its starting position in the input string.
-pub fn get_kmer_minimizers<'a>(seq: &'a str, k_size: usize, w_size: usize, mut minimizers:  Vec<usize> )  {
-    let mut w= 0;
-    if w_size > k_size{
-        w = w_size - k_size;
-    }
-    else {
-        w = 1;
-    }
-    let mut window_kmers: VecDeque<(&'a str, usize)> = VecDeque::with_capacity(w + 1);
-    if w+ k_size < seq.len() + 1{
-        for i in 0..w {
-            window_kmers.push_back((&seq[i..i + k_size], i));
-        }
-    }
-    // Initialize the window_kmers deque
-    else if seq.len() + 1 >= k_size{
-        let short_w = seq.len() + 1 - k_size;
-        for i in 0..short_w {
-            window_kmers.push_back((&seq[i..i + k_size], i));
-        }
-    }
-
-    //store the final positional minimizers in a vector
-    //let mut minimizers = vec![];
-    // Find the initial minimizer (minimizer of initial window)
-    let (curr_min, min_pos) = window_kmers.iter().min_by_key(|&&(kmer, _)| kmer).unwrap();
-    //add the initial minimizer to the vector
-    let mini =  *min_pos;
-    minimizers.push(mini);
-    //we always store the previous minimizer to compare to the newly found one
-    let mut prev_minimizer =(*min_pos,*curr_min);
-    //iterate further over the sequence and generate the minimizers thereof
-    for (i, new_kmer) in seq[w..].as_bytes().windows(k_size).enumerate() {
-        let new_kmer_pos = i  + w;
-        let new_kmer_str = std::str::from_utf8(new_kmer).unwrap();
-        // updating window
-        window_kmers.pop_front().unwrap();
-        window_kmers.push_back((new_kmer_str, new_kmer_pos));
-        // Find the new minimizer
-        let (curr_min, min_pos) = window_kmers.iter().min_by_key(|&&(kmer, _)| kmer).unwrap();
-        //make sure that the minimal string is a new minimizer not just the previously found one
-        if  *min_pos != prev_minimizer.0{ //&& *curr_min != prev_minimizer.1 {
-            //add the minimizer into the vector and store the minimizer as previously detected minimizer
-            let mini =  *min_pos ;
-            //println!("minimizer {:?}",mini);
-            minimizers.push(mini);
-            prev_minimizer=(*min_pos, *curr_min);
-        }
-    }
-
-}
-
-pub fn is_significant(quality_interval: &str, d_no_min:[f64;128],quality_threshold: &f64)->bool{
-    let mut significance_indicator= false;
-    let mut qualities :Vec<f64> = vec![];
-    let mut quality = 1.0;
-    let mut index;
-    let mut q_value;
-    let mut probability_error;
-    //for each character in quality string:
-    for (i, c) in quality_interval.chars().enumerate() {
-        index = c as usize;
-        //q_value gives the PHRED quality score: i.e. '+' gives us 0.1
-        q_value = d_no_min[index];
-        //here we get the base call accuracy
-        probability_error = 1.0 - q_value;
-        qualities.push(probability_error);
-        quality *= probability_error
-    }
-    if quality > *quality_threshold {
-        significance_indicator = true;
-    }
-    significance_indicator
-}
-
-pub fn get_canonical_kmer_minimizers_hashed(seq: &str, k_size: usize, w_size: usize, this_minimizers: &mut Vec<usize>)  {
-    //make sure that we have suitable values for k_size and w_size (w_size should be larger)
-    let mut w= 0;
-    if w_size > k_size{
-        w = w_size - k_size;
-    }
-    //k_size was chosen larger than w_size. To not fail we use every k-mer as minimizer (maybe have an error message?)
-    else {
-        w = 1;
-    }
-    //let mut rc_vec=VecDeque::with_capacity(w+1);
-    let mut window_kmers: VecDeque<(u64, usize)> = VecDeque::with_capacity(w + 1);
-    let mut k_mer_str;
-    let mut rc_string;
-    let mut forward_hash;
-    let mut reverse_hash;
-    //we can only get a minimizer if the sequence is longer than w + k_size - 1 (else we do not even cover one full window)
-    if w + k_size < seq.len() + 1{
-        for i in 0..w {
-            k_mer_str = &seq [i..i + k_size];
-            rc_string = clustering::reverse_complement(k_mer_str).clone();
-
-            //generate the hashes of the kmers
-            forward_hash = generate_sorted_fastq_new_version::calculate_hash(&k_mer_str.to_string());
-            reverse_hash = generate_sorted_fastq_new_version::calculate_hash(&rc_string.to_string());
-            //we now want to find the canonical minimizer: we only push the smaller k-mer of k_mer_str and rc_String into the window
-            if forward_hash <= reverse_hash {
-                window_kmers.push_back((forward_hash, i));
-            }
-            else{
-                window_kmers.push_back((reverse_hash, i))
-            }
-
-        }
-    }
-    //println!("kmers in window: {:?}", window_kmers);
-    //store the final positional minimizers in a vector
-    if !window_kmers.is_empty(){
-        // Find the initial minimizer (minimizer of initial window)
-        let mut binding=window_kmers.clone();
-        let (curr_min, min_pos) = binding.iter().min_by_key(|&(kmer, _)| kmer).unwrap();
-        //add the initial minimizer to the vector
-        let mut mini = *min_pos ;
-        this_minimizers.push(mini.clone());
-        //we always store the previous minimizer to compare to the newly found one
-        let mut prev_minimizer = mini;
-        let mut new_kmer_pos;
-        let mut new_kmer_str;
-        let mut rc_string;
-        let mut forward_hash;
-        let mut reverse_hash;
-        //iterate further over the sequence and generate the minimizers thereof
-        for (i, new_kmer) in seq[w..].as_bytes().windows(k_size).enumerate() {
-            new_kmer_pos = i  + w;
-            new_kmer_str = std::str::from_utf8(new_kmer).unwrap();
-            rc_string = clustering::reverse_complement(new_kmer_str).clone();
-            // updating  by removing first kmer from window
-            window_kmers.pop_front().unwrap();
-            forward_hash=generate_sorted_fastq_new_version::calculate_hash(&new_kmer_str.to_string());
-            reverse_hash=generate_sorted_fastq_new_version::calculate_hash(&rc_string.to_string());
-            if reverse_hash > forward_hash{
-                window_kmers.push_back((forward_hash, new_kmer_pos));
-            }
-            else {
-                window_kmers.push_back((reverse_hash,new_kmer_pos))
-            }
-            // Find the new minimizer, we need a ds that was cloned from window_kmers to abide ownership rules in rust
-            binding = window_kmers.clone();
-            let (curr_min, min_pos) = *binding.iter().min_by_key(|&(kmer, _)| kmer).unwrap();
-            //make sure that the minimal string is a new minimizer not just the previously found one
-            if  min_pos != prev_minimizer{ //&& *curr_min != prev_minimizer.1 {
-                //add the minimizer into the vector and store the minimizer as previously detected minimizer
-                mini = min_pos ;
-                //println!("minimizer {:?}",mini);
-                this_minimizers.push(mini.clone());
-                prev_minimizer = mini.clone();
-            }
-            rc_string.clear();
-        }
-    }
-}
-pub fn filter_minimizers_by_quality(this_minimizers: &Vec<usize>, fastq_quality:&str, k: usize, d_no_min:[f64;128], minimizers_filtered: &mut Vec<usize>, quality_threshold: &f64) {
-    let mut skipped_cter= 0;
-    let mut minimizer_range_start;
-    let mut significant= false;
-    for mini in this_minimizers{
-        minimizer_range_start = *mini;
-        let qualitiy_interval = &fastq_quality[minimizer_range_start..minimizer_range_start+k];
-        significant = is_significant(qualitiy_interval, d_no_min, quality_threshold);
-        if significant{
-            minimizers_filtered.push(mini.clone())
-        }
-        else{
-            skipped_cter += 1;
-        }
-    }
-    //println!("{} minimizers filtered out due to bad quality", skipped_cter);
-    //println!("Length after filter: {}",minimizers_filtered.len());
-    //minimizers_filtered
-}
-
-
 
 
 fn analyse_fastq_and_sort(k:usize, q_threshold:f64, in_file_path:&str, quality_threshold: &f64, window_size: usize, score_vec: &mut Vec<(i32,usize)>, id_map: &mut FxHashMap<i32,String>){
     /*
     Reads, filters and sorts reads from a fastq file so that we are left with reads having a reasonable quality score, that are sorted by score
      */
-    //read the fastq file and store the result in fastq_records (a vector of FastqRecord_isoncl_init)
-    //let mut score_vec=vec![];
     let d_no_min= compute_d_no_min();
-    //let mut id_map = FxHashMap::default();
-    //let mut reader = fastq::Reader::new(in_file_path);
-    //let fastq_file = File::open(in_file_path).unwrap();
+    //read_id holds the internal id we appoint to a read
     let mut read_id = 0;
+    //generate a Reader object that parses the fastq-file (taken from rust-bio)
     let mut reader = fastq::Reader::from_file(Path::new(&in_file_path)).expect("We expect the file to exist");
-    //let mut reader = parse_fastx_file(&filename).expect("valid path/file");
+    //iterate over the records
     for record in reader.records().into_iter(){
         let seq_rec = record.expect("invalid record");
         let header_new = seq_rec.id();
@@ -347,48 +137,11 @@ fn analyse_fastq_and_sort(k:usize, q_threshold:f64, in_file_path:&str, quality_t
             }
         }
     }
-    println!("{:?}",score_vec.pop());
+    //sort the score_vec by the number of high-confidence seeds (most to least)
     score_vec.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());//TODO: replace by par_sort_by
     println!("{} reads accepted",score_vec.len());
-    println!("{:?}",score_vec.pop());
+    //println!("{:?}",score_vec.pop());
 }
-
-/*fn analyse_fastq_and_sort_old(k:usize, q_threshold:f64, in_file_path:&str, quality_threshold: &f64, window_size: usize, fastq_records: &mut Vec<FastqRecord_isoncl_init>){
-    /*
-    Reads, filters and sorts reads from a fastq file so that we are left with reads having a reasonable quality score, that are sorted by score
-     */
-    //read the fastq file and store the result in fastq_records (a vector of FastqRecord_isoncl_init)
-    let d_no_min= compute_d_no_min();
-    //let mut reader = fastq::Reader::new(in_file_path);
-    let fastq_file = File::open(in_file_path).unwrap();
-    file_actions::parse_fastq(fastq_file, fastq_records);
-    println!("{} reads recorded",fastq_records.len());
-    //filter fastq_records: We only keep reads having a sequence length>2*k and that do not have a shorter compression than k
-    fastq_records.retain(|record| record.get_sequence().len() >= 2*k && compress_sequence(&*record.get_sequence()).len() >= k );
-    //Test does not function yet: fastq_records.into_par_iter().filter(|record| record.get_sequence().len() >= 2*k && compress_sequence(&*record.get_sequence()).len() >= k );
-    println!("{} reads accepted",fastq_records.len());
-    //compute d_no_min and d, two arrays that we use for the calculations
-
-    //iterate over all fastq_records and calculate error_rate as well as score
-    //while let Some(Ok(record)) = records.next() {
-    fastq_records.iter_mut().for_each(|fastq_record| {//TODo replace with par_iter_mut again
-        let mut this_minimizers=vec![];
-        let mut filtered_minis = vec![];
-        get_canonical_kmer_minimizers_hashed(fastq_record.get_sequence(), k, window_size,&mut this_minimizers);
-        filter_minimizers_by_quality(&this_minimizers, &fastq_record.get_quality(),k,d_no_min,&mut filtered_minis, quality_threshold);
-        //calculate the error rate and store it in vector errors
-        fastq_record.set_error_rate( calculate_error_rate(&fastq_record.get_quality(), &d_no_min));
-        //calculate the final score and add it to fastq_record (we have a dedicated field for that that was initialized with 0.0)
-        fastq_record.set_score(filtered_minis.len() as f64)
-    });
-    //filter out records that have a too high error rate
-    fastq_records.retain(|record| 10.0_f64 * -record.get_err_rate().log(10.0_f64) > q_threshold);
-
-    println!("{} reads accepted",fastq_records.len());
-    //sort the vector fastq_records by scores
-    fastq_records.sort_by(|a, b| b.get_score().partial_cmp(&a.get_score()).unwrap());//TODO: replace by par_sort_by
-    //fastq_records.reverse();
-}*/
 
 
 fn print_statistics(fastq_records:&Vec<FastqRecord_isoncl_init>){
@@ -407,14 +160,18 @@ fn print_statistics(fastq_records:&Vec<FastqRecord_isoncl_init>){
 pub(crate) fn sort_fastq_for_cluster(k:usize, q_threshold:f64, in_file_path:&str,outfolder: &String, quality_threshold:&f64,window_size: usize) {
     println!("Sorting the fastq_file");
     let now = Instant::now();
+    //holds the internal ids and scores as tuples to be able to sort properly
     let mut score_vec=vec![];
+    //holds the internal read id
     let mut id_map=FxHashMap::default();
+    //the main step of the sort_fastq_for_cluster step: Gets the number of high-confidence seeds for each read and writes them into score_vec
     analyse_fastq_and_sort(k, q_threshold, in_file_path,quality_threshold,window_size,&mut score_vec, &mut id_map);
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
     if !path_exists(&outfolder){
         fs::create_dir(outfolder.clone()).expect("We should be able to create the directory");
     }
+    //write a fastq-file that contains the reordered reads
     write_output::write_ordered_fastq(&score_vec, outfolder,&id_map,in_file_path);
     println!("Wrote the sorted fastq file");
     //print_statistics(fastq_records.borrow());
--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,8 @@ use bio::io::fasta;
 use bio::io::fasta::FastaRead;
 use bio::io::fastq;
 use bio::io::fastq::FastqRead;
-
+use std::convert::TryFrom;
+use crate::gtf_handling::resolve_gtf;
 
 
 
@@ -127,143 +128,8 @@ fn filter_fastq_records(mut fastq_records:Vec<FastqRecord_isoncl_init>,d_no_min:
 }
 
 
-/*fn main() {
-    let args: Vec<String> = env::args().collect();
-    //let fasta_path = &args[1];
-    let fastq_path = &args[1];
-    let mut initial_clustering_records=vec![];
-    if args.len()>1{
-        let initial_clustering_path = &args[2];
-        let initial_clustering_file = File::open(initial_clustering_path).unwrap();
-        initial_clustering_records = file_actions::parse_fasta(initial_clustering_path).unwrap();
-
-    }
-
-    let k=9;
-    let window_size = 20;
-
-    //let fasta_file = File::open(fasta_path).unwrap();
-    //let fasta_records = FileActions::parse_fasta(fasta_file).unwrap();
-    let fastq_file = File::open(fastq_path).unwrap();
-
-    let fastq_records = file_actions::parse_fastq(fastq_file).unwrap();
-
-    //test_minimizer_gens();
-    //let mut mini_map: HashMap<i32, Vec<generate_sorted_fastq_new_version::Minimizer>> = HashMap::with_capacity(fastq_records.len());
-    let mut mini_map_filtered: HashMap<i32, Vec<generate_sorted_fastq_new_version::Minimizer>> = HashMap::with_capacity(fastq_records.len());
-
-    //let mut idmap: HashMap<&str, i32> = HashMap::with_capacity(fastq_records.len());
-    //let mut iterator=fastq_records.iter();
-    for fastq_record in fastq_records{
-        let this_minimizers = generate_sorted_fastq_new_version::get_kmer_minimizers(&*fastq_record.get_sequence(), k, window_size);
-        //mini_map.insert(fastq_record.internal_id, this_minimizers.clone());
-        let filtered_minis = generate_sorted_fastq_new_version::filter_minimizers_by_quality(this_minimizers,&fastq_record.sequence, &fastq_record.quality,window_size,k);
-        mini_map_filtered.insert(fastq_record.internal_id,filtered_minis);
-    }
-    //sorted_entries: a Vec<(i32,Vec<Minimizer)> sorted by the number of significant minimizers: First read has the most significant minimizers->least amount of significant minimizers
-    let sorted_entries = get_sorted_entries(mini_map_filtered);
-
-    if initial_clustering_records.len() > 0{
-        let init_cluster_map= clustering::get_initial_clustering(initial_clustering_records,k,window_size);
-        clustering::cluster_from_initial(sorted_entries, init_cluster_map);
-    }
-    else{
-        clustering::cluster_sorted_entries(sorted_entries);
-    }
-    let d_no_min =generate_sorted_fastq_new_version::compute_d_no_min();
-    println!("{:?}",d_no_min)
 
 
-    //generate_sorted_fastq_for_cluster::sort_fastq_for_cluster(15,7.0,"/home/alexanderpetri/Rust/100k_sample.fastq")
-    //let seq=dna!("ACTGACTACACAT");
-    //let dna_sequence = "ATCGA";
-    //let reversed_complement = generate_sorted_fastq_for_cluster::reverse_complement(dna_sequence);
-    //println!("Reverse complement: {}", reversed_complement);
-
-    /*for rec in fasta_records {
-        println!("{}", rec);
-    }
-    for rec in fastq_records {
-        println!("{}",rec);
-    }*/
-
-
-
-
-
-
-    /*
-    THREAD test field: here I test how to use threads in Rust to actually make it possible to run several threads in parallel
-
-     */
-    /*let handle=thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-    handle.join().unwrap();
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
-    let threads: Vec<_> = (0..500)
-        .map(|i| {
-            thread::spawn(move || {
-                println!("Thread #{} started!",i);
-                thread::sleep(Duration::from_millis(5000));
-                println!("Thread #{} finished!",i);
-            })
-        })
-        .collect();
-
-    for handle in threads {
-        handle.join().unwrap();
-    }*/
-
-}
-*/
-
-/*fn main() {
-    let matches = command!() // requires `cargo` feature
-        .propagate_version(true)
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        .subcommand(
-            Command::new("index_kmers")
-                .arg(
-                    arg!([FASTQ])
-                        .value_parser(value_parser!(String))
-                        .long("fastq")
-                        .help("Path to consensus fastq file(s)")
-                )
-                .arg(
-                    arg!([K])
-                        .value_parser(value_parser!(u32))
-                        .short('k')
-                        .default_value("15")
-                        .help("kmer size")
-                )
-                .arg(
-                    arg!([T])
-                        .value_parser(value_parser!(u32))
-                        .short('t')
-                        .default_value("8")
-                        .help("kmindex number of threads")
-                )
-        )
-        .get_matches();
-        match matches.subcommand() {
-        Some(("index_kmers", sub_matches)) => {
-        index_kmers(&sub_matches)
-        }
-    _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
-    }
-
-}*/
-
-use std::convert::TryFrom;
-use crate::gtf_handling::resolve_gtf;
 
 fn convert_cl_id(v: usize) -> Option<i32> {
     i32::try_from(v).ok()
