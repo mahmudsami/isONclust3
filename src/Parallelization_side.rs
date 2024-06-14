2c9525db35727a7ccaa22a0e8ef08ce557232846
aljpetri <aljpetri@gmail.com>
2024-06-14 10:33:55 +0300

    Decided to split the files during read sorting

--- src/Parallelization_side.rs
+++ src/Parallelization_side.rs
@@ -1,10 +1,22 @@
 
+use bio::io::fastq;
+use bio::io::bed::Record;
+use std::path::Path;
+use rayon::prelude::*;
 
-fn split_in_batches(n_threads: usize, data: &[u8])-> Vec<Vec<u8>>{
-    let each_len= data.len() / n_threads + if data.len() % n_threads == 0 {0} else {1};
+
+fn split_in_batches(n_threads: usize, score_vec: Vec<(i32,usize)>)-> Vec<Vec<i32>>{
+
+    //let records = fastq::Reader::from_file(Path::new(filename))
+    //    .records()
+    //    .map(|record| record.unwrap());
+    let each_len = score_vec.len() / n_threads + if score_vec.len() % n_threads == 0 {0} else {1};
     let mut out = vec![Vec::with_capacity(each_len); n_threads];
-    for(i,d) in data.iter().copied().enumerate(){
-        out[i % n_threads].push(d);
+    //let mut reader = fastq::Reader::from_file(Path::new(filename)).expect("We expect the file to exist");
+    for read_info in score_vec.into_iter() {
+        /*for(i,d) in data.iter().copied().enumerate(){
+       out[i % n_threads].push(d);
+   }*/
     }
     out
 }
